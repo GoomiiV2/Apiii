62ed34b12b53cabd8c2507a734b50bb9580697f1
Arkii <kneehighpeas@gmail.com>
2014-01-30 17:50:21 +0000

    Stuff

--- Server/notepad++AutoComplete.php
+++ Server/notepad++AutoComplete.php
@@ -8,12 +8,45 @@
 		.'	<AutoComplete language="LUA">'.PHP_EOL
 		.'		<Environment ignoreCase="no" startFunc="(" stopFunc=")" paramSeparator="," terminal=";" additionalWordChar = "."/>'.PHP_EOL;
 
-        // Well this got messy :<
 		foreach ($data as $key => $value)
 		{
 			foreach ($value as $k => $val)
 			{
-                PrintFunctionDef($k, $val);
+				if (is_array($val))
+				{
+					if (array_key_exists("usageString", $val))
+						$desc = $val["usageString"];
+					if (array_key_exists("returnVal", $val))
+						$retVal = $val["returnVal"];
+
+					$str = $str. '		<KeyWord name="'.$key.".".$k.'" func="yes">'.PHP_EOL;
+
+					if ($desc && $retVal)
+						$str = $str. '			<Overload retVal="'.$retVal.'" descr="'.$desc.'">'.PHP_EOL;
+					elseif ($desc)
+						$str = $str. '			<Overload retVal="" descr="'.$desc.'">'.PHP_EOL;
+					elseif ($retVal)
+						$str = $str. '			<Overload retVal="'.$retVal.'" descr="">'.PHP_EOL;
+
+					if (array_key_exists("args", $val))
+						foreach ($val["args"] as $arg)
+						{
+							$str = $str. '				<Param name="'.$arg.'"/>'.PHP_EOL;
+						}
+
+
+					if ($desc || $retVal)
+						$str = $str. '			</Overload>'.PHP_EOL;
+
+					$str = $str. '		</KeyWord>'.PHP_EOL;
+
+					$desc = null;
+					$retVal = null;
+				}
+				else
+				{
+					$str = $str. '		<KeyWord name="'.$key.".".$val.'"/>'.PHP_EOL;
+				}
 			}
 		}
 		
@@ -25,52 +58,4 @@
 		
 		file_put_contents($file, $str);
 	}
-
-    function PrintFunctionDef($key, $k, $val)
-    {
-        if (is_array($val))
-        {
-            if (array_key_exists("usageString", $val))
-                $desc = $val["usageString"];
-            if (array_key_exists("returnVal", $val))
-                $retVal = $val["returnVal"];
-
-            $str = $str. '		<KeyWord name="'.$key.".".$k.'" func="yes">'.PHP_EOL;
-
-
-
-            PrintArgs($val);
-
-            if ($desc || $retVal)
-                $str = $str. '			</Overload>'.PHP_EOL;
-
-            $str = $str. '		</KeyWord>'.PHP_EOL;
-
-            $desc = null;
-            $retVal = null;
-        }
-        else
-        {
-            $str = $str. '		<KeyWord name="'.$key.".".$val.'"/>'.PHP_EOL;
-        }
-    }
-
-function PrintOverLoad()
-{
-    if ($desc && $retVal)
-        $str = $str. '			<Overload retVal="'.$retVal.'" descr="'.$desc.'">'.PHP_EOL;
-    elseif ($desc)
-        $str = $str. '			<Overload retVal="" descr="'.$desc.'">'.PHP_EOL;
-    elseif ($retVal)
-        $str = $str. '			<Overload retVal="'.$retVal.'" descr="">'.PHP_EOL;
-}
-
-function PrintArgs($val)
-{
-    if (array_key_exists("args", $val))
-        foreach ($val["args"] as $arg)
-        {
-            $str = $str. '				<Param name="'.$arg.'"/>'.PHP_EOL;
-        }
-}
 ?>
